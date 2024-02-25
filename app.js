336d6cc3cbb081fb0b51c5097c01a57a33a87de4
Goran Medakovic <gomedako@gmail.com>
2024-02-25 19:22:19 +0100

    Created basic structure with random computer choice in js

---
+++ app.js
@@ -0,0 +1,15 @@
+const choiceList = ["rock", "paper", "scissor"];
+
+const getComputerChoice = () => {
+  const randomIndex = Math.floor(Math.random() * choiceList.length);
+  return choiceList[randomIndex];
+};
+
+console.log(getComputerChoice());
+
+const getPlayerChoice = () => {
+  userChoice = prompt("Write - rock, paper or scisscors");
+  if 
+};
+
+getPlayerChoice();
